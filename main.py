a80a9d167314e30e111f295b605758f9ff3a23ae
ahmadreza.al <ahmadreza.al@behinrahkar.com>
2025-05-20 14:14:12 +0330

    testing fast api for start

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
